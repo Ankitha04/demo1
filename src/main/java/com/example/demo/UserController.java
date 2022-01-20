f113b1dacdd8acd719248d13545036f92fb9ff9f
Ankitha04 <tcslearning06@gmail.com>
2022-01-20 12:32:40 +0530

    added database integration

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
+import com.example.demo.service.IUserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class UserController {
+	@Autowired
+	IUserService userService;
     @GetMapping("/user")
 	String  getUser() {
     	System.out.println("testing");
@@ -21,7 +25,7 @@ public class UserController {
 	@PostMapping("/user") //create
 	void createUser(@RequestBody User user) {
 		System.out.println(user.getName());
-			
+		userService.saveUser(user);	
 		}
 	@DeleteMapping("/user")
 	void deleteuser() {
