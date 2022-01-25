fc1b98d38c1e85acb0f821f38073d709d649dbd7
Ankitha04 <tcslearning06@gmail.com>
2022-01-25 11:32:41 +0530

    adress

---
+++ src/main/java/com/example/demo/entity/Address.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Integer id;
+	String house;
+	public String getHouse() {
+		return house;
+	}
+	public void setHouse(String house) {
+		this.house = house;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	String street;
+
+}
